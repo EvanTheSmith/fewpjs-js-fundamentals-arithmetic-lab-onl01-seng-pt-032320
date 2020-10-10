77ffc2cb4b8bba949c022fb5ed7f6cb2b1bddbd6
EvanTheSmith <ProfessionalEmail@Zoho.com>
2020-10-10 03:25:55 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+// Test 1
+let num1=31; let num2=2; let multiply = num1 * num2;
+
+// Test 2
+let random = Math.random() + 1;
+
+// Test 3
+let mod = 24 % 5;
+
+// Test 4
+let max = Math.max(1, 2, 3, 4, 20);
